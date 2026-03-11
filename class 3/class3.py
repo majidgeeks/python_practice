298c101a6620ff1a42a2dd64759d63727a5e126a
Majid Hussain <majidrajput246@gmail.com>
2026-03-12 03:46:46 +0500

    continue

--- class 3/class3.py
+++ class 3/class3.py
@@ -21,11 +21,70 @@
 #     def masalaPizza(self):
 #         self.quatity += 1
 
+# list methods
 
-cart = ["Pizza", "Burger"]
-cart.append('Fires')
-print(cart)
+# cart = ["Pizza", "Burger"]
+# cart.append('Fires')
+# print(cart)
 
         
+# cart = ["Pizza", "Burger"]
+# new_items = ["Fries", "Drink"]
 
-                
+# cart.extend(new_items)
+
+# print(cart)
+
+# cart = ["Pizza", "Burger"]
+# cart.insert(2, 'Leg Tikka')
+# print(cart)        
+# 
+
+# cart = ["Pizza", "Burger", "Fries"]
+# cart.remove('Pizza')
+# print(cart)
+
+
+# cart = ["Pizza", "Burger", "Fries"]
+# cart.pop(0)
+# print(cart)
+
+# cart = ["Pizza", "Burger"]
+# cart.clear()
+# print(cart)
+
+# cart = ["Pizza", "Burger", "Fries"]
+# print(cart.index('Pizza'))
+
+# cart = []
+
+# cart.append('Pizza')
+
+# cart.insert(0,'Fries')
+# print(cart)
+
+# cart.insert(0, 'Tikka')
+# print('again insert',cart)
+
+# cart.remove('Fries')
+# print('after remove fries',cart)
+
+# cities = ["Atlanta", "Baltimore", "Chicago", "Angeles", "Seattle"]
+
+# print(cities[0])
+# cities.append(10)
+# print(cities)
+
+# todays_tasks = []
+# todays_tasks = todays_tasks + ["Walk dog", "Buygroceries"]
+# print(todays_tasks)
+
+# cities = ["Atlanta", "Baltimore", "Chicago", "Angeles", "Seattle"]
+# newCities = cities[2:]
+# print(newCities)
+
+
+states_in_order_of_founding = ("Delaware",
+"Pennsylvania", "New Jersey", "Georgia")
+
+print(states_in_order_of_founding[1])
---
+++ forLoops/class4.py
@@ -0,0 +1,48 @@
+# cleanest_cities = ["Cheyenne", "Santa Fe", "Tucson","Great Falls", "Honolulu"]
+# city_to_check = "Tucson"
+
+# for i in cleanest_cities:
+#     if i == city_to_check:
+#         print('got it')
+#         break
+#     else:
+#         print('not available')    
+
+# first_names = ["BlueRay ", "Upchuck ", "Lojack ","Gizmo ", "Do-Rag "]
+# last_names = ["Zzz", "Burp", "Dogbone", "Droop"]
+# fullName = []
+
+# for a in first_names:
+#     for b in last_names:
+#         fullName.append(a + " " + b)
+
+# print(fullName)        
+
+
+obj1 = {
+    'name': 'majid',
+    'class': 'twelve',
+    'domain': 'develop' 
+}
+
+# print(obj1[2])
+# obj1['class'] = 14
+
+# print(obj1['class'])
+obj1['greet'] = 'salam'
+
+# print(obj1)
+# del obj1[0]
+# print(obj1)
+
+# for values in obj1.values():
+#     print('values ',values)
+
+# for shaka in obj1.keys():
+#     print('shaka keys',shaka)    
+
+# for itemWala in obj1.items():
+#     print('itemWala',itemWala)    
+
+# for a , b in obj1.items():
+#     print('the raj is ' + a + ' and vala bhi is' + b)
